d2321792fdba543a0b90b2c58b1a01abaa16a27f
YesPeterReal <dpeterojo@gmail.com>
2025-08-16 19:53:04 +0200

    Install Tailwind 3.4.1 and configure for BankOS styling in portal

---
+++ apps/portal/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'gray-900': '#1a202c',
+        'gray-400': '#9ca3af',
+        'gray-300': '#d1d5db',
+        'green-400': '#68d391',
+      },
+    },
+  },
+  plugins: [],
+};
